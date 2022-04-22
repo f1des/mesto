3b68118389c1476c94f2dd9246f47ca73ae32243
Anton <samsonow2014@yandex.ru>
2022-04-23 01:39:39 +0400

    chore: Написать функцию

--- script/index.js
+++ script/index.js
@@ -1,1 +1,19 @@
-let container = document.querySelector('.page');
+// let page = document.querySelector('.page');
+// let popup_opened = page.querySelector('.popup');
+
+// function openPopap() {
+//   /*popup_opened.setAttribute('disabled', true); */
+//   popup_opened.classList.add('.popup_opened');
+
+//   console.log('Мы кликнули по элементу');
+// }
+
+// // popup_opened.addEventListener('click', openPopap);
+// let page = document.querySelector('.page');
+// let openP = page.querySelector('.popup');
+
+let btn = document.querySelector('.profile__add-btn');
+let par = document.querySelector('.popup');
+btn[0].onclick = function() {
+  par[0].classList.add("popup_opened");
+}
