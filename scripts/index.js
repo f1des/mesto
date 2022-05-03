fedab18e53660d51d83b3588c5b636e1c984952c
f1des <samsonow2014@yandex.ru>
2022-05-03 15:55:31 +0400

    feat: Создать файл initialcards.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,44 @@
 window.addEventListener('DOMContentLoaded', function init () {
-  window.removeEventListener('DOMContentLoaded', init)
-    const profile = document.querySelector('.profile');
-    const profileName = profile.querySelector('.profile__name');
-    const profileJob = profile.querySelector('.profile__job');
-    const editBtn = profile.querySelector('.profile__edit-btn');
+  window.removeEventListener('DOMContentLoaded', init);
 
-    const popups = document.querySelector('.popup');
-    const closeBtn = popups.querySelector('.popup__close-btn');
-    const form = popups.querySelector('.popup__form');
-    const { name:textName, job:textJob } = form.elements;
+  import initialCards from './initialCards.js';
 
-    function showPopup () {
-      popups.classList.add('popup_opened');
-      if (textName.value !== profileName.textContent) {
-        textName.value = profileName.textContent;
-      }
-      if (textJob.value !== profileJob.textContent) {
-        textJob.value = profileJob.textContent;
-      }
+  const profile = document.querySelector('.profile');
+  const profileName = profile.querySelector('.profile__name');
+  const profileJob = profile.querySelector('.profile__job');
+  const editBtn = profile.querySelector('.profile__edit-btn');
+
+  const popups = document.querySelector('.popup');
+  const closeBtn = popups.querySelector('.popup__close-btn');
+  const form = popups.querySelector('.popup__form');
+  const { name:textName, job:textJob } = form.elements;
+
+  function showPopup () {
+    popups.classList.add('popup_opened');
+    if (textName.value !== profileName.textContent) {
+      textName.value = profileName.textContent;
     }
-    
-    editBtn.addEventListener('click', showPopup);
-
-    function closePopup () {
-      popups.classList.remove('popup_opened');
+    if (textJob.value !== profileJob.textContent) {
+      textJob.value = profileJob.textContent;
     }
+  }
+  
+  editBtn.addEventListener('click', showPopup);
 
-    closeBtn.addEventListener('click', closePopup);
+  function closePopup () {
+    popups.classList.remove('popup_opened');
+  }
 
-    // Сохраняем данные профиля при закрытии попапа. Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-    function saveDataProfile(evt) {
-      evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-      profileName.textContent = textName.value;
-      profileJob.textContent = textJob.value;
-      closePopup(popups);
-    }
+  closeBtn.addEventListener('click', closePopup);
 
-    // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-    form.addEventListener('submit', saveDataProfile);
+  // Сохраняем данные профиля при закрытии попапа. Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+  function saveDataProfile(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    profileName.textContent = textName.value;
+    profileJob.textContent = textJob.value;
+    closePopup(popups);
+  }
+
+  // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+  form.addEventListener('submit', saveDataProfile);
 })
