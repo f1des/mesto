dbecee6147f745746f9760b77fadcd09b5d33ece
Anton <samsonow2014@yandex.ru>
2022-05-14 22:25:56 +0400

    feat: Функция делегирования дописать

--- scripts/index.js
+++ scripts/index.js
@@ -12,22 +12,21 @@ window.addEventListener('DOMContentLoaded', function init () {
   const editPopup = document.querySelector('#popupEditProfile');
   const addPopup = document.querySelector('#popupAddPhoto');
 
-  const closeBtn = document.querySelectorAll('.popup');
-  const popups = document.querySelector('.popup');
-  //const closeBtn = popups.querySelector('.popup__close-btn');
+  const closeBtn = document.querySelectorAll('.popup__close-btn');
 
+  const popups = document.querySelectorAll('.popup');
   // Объявления для формы редактирования и добавления карточек
-  const formEditProfile = popups.querySelector('.popup__form');
+  const formEditProfile = document.querySelector('.popup__form');
   const { name:textName, job:textJob } = formEditProfile.elements;
-  const formAddPhoto = popups.querySelector('.popup__form');
-  //const { title:textTitle, link:textLink } = formAddPhoto.elements;
+  const formAddPhoto = document.querySelector('.popup__form');
+  const { title:textTitle, link:textLink } = formAddPhoto.elements;
 
   function showPopup (popups) {
     popups.classList.add('popup_opened');
   }
 
-  function closePopup () {
-    popups.classList.remove('popup_opened');
+  function closePopup (popup) {
+    popup.classList.remove('popup_opened');
   }
 
   const modifyDataInProfile = () => {
@@ -44,25 +43,25 @@ window.addEventListener('DOMContentLoaded', function init () {
     modifyDataInProfile();
   }
 
-  function showAddProfilePopup () {
+  function showAddPhotoPopup () {
     showPopup(addPopup);
   }  
   
   editBtn.addEventListener('click', showEditProfilePopup);
-  addBtn.addEventListener('click', showAddProfilePopup);
+  addBtn.addEventListener('click', showAddPhotoPopup);
 
   // Сохраняем данные профиля при закрытии попапа. Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
   function saveDataProfile(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     profileName.textContent = textName.value;
     profileJob.textContent = textJob.value;
-    closePopup;
+    closePopup(popups);
   }
 
    // Сохраняем данные профиля при закрытии попапа. Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
    function saveDataPhoto(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    closePopup;
+    closePopup(popups);
   }
 
   // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
@@ -70,14 +69,12 @@ window.addEventListener('DOMContentLoaded', function init () {
   formAddPhoto.addEventListener('submit', saveDataPhoto);
 
   //Функция делегирования событий
-  document.querySelectorAll('.popup__close-btn').forEach(item => {
-    item.addEventListener('click', () => {
-      closePopup();
-      console.log('1');
+  popups.forEach((popup) => {
+    popup.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('popup__close-btn')) {
+        closePopup(popup);
+      }
     });
   });
-  
-
-
 
 })
