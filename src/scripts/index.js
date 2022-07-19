d713535542b8c958cd7978741db6720d0f5861bc
Anton <samsonow2014@yandex.ru>
2022-07-19 14:30:33 +0400

    feat: Настроить Webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+import '../pages/index.css';
 import { initialCards, objValidation, templateSelector } from './constants.js';
 import FormValidator from './FormValidator.js';
 import Card from './Card.js';
